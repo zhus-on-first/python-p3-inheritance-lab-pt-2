cb62e0280b07a09679a96ad9fb6f71b1461f1566
Jia-Jia Zhu <135554427+zhus-on-first@users.noreply.github.com>
2023-09-13 14:51:00 -0700

    nice

--- lib/student.py
+++ lib/student.py
@@ -1,5 +1,24 @@
 class Student:
-    pass
+    def hello(self):
+        print("Hey there! I'm so excited to learn stuff.")
+
+    def raise_hand(self):
+        print("Pick me!")
 
 class ChattyStudent(Student):
-    pass
+    def hello(self):
+        super().hello()
+        print("How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died...")
+
+    def raise_hand(self):
+        super().raise_hand()
+        super().raise_hand()
+        super().raise_hand()
+        super().raise_hand()
+        super().raise_hand()
+        super().raise_hand()
+        super().raise_hand()
+        super().raise_hand()
+        super().raise_hand()
+        super().raise_hand()
+    
